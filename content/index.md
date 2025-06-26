cd4987a91da65233fab43e87b532076707ec9be2
DavidBMatejka <david.b.matejka@gmail.com>
2025-06-26 17:48:07 +0200

    Quartz sync: Jun 26, 2025, 5:48 PM

--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: Welcome to Quartz
+title: Programmieren in Processing
 ---
 ## IMP 8
 [[Arrays]]
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "IMP - Programmieren in Processing",
+    pageTitle: "IMP - Processing",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
